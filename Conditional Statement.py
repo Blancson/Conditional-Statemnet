1adba8af177a571a8a660817de5b58f24d618aa1
Godwin Okemena <99687826+Blancson@users.noreply.github.com>
2022-10-03 16:20:29 +0100

    Add files via upload

---
+++ Conditional Statement.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Enter a value for points.
+points = int(input('Enter points: '))
+if points <= 50:
+    result = "Congratulations! You won a wooden rabbit"
+elif points >= 51 and points <= 150:
+    result = "Oh dear, no prize this time."
+elif points >= 151 and points <= 180:
+    result = "Congratulations! You won a wafer-thin mint"
+elif points >= 181 and points <= 200:
+    result = "Congratulations! You won a penguin"
+else:
+    result = "input a valid point"
+print(result)
+
